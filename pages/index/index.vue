7ba3924089f1fd12edb161d15e89d5faa735af10
youxiaobei666 <youxiaobei666@gmail.com>
2023-03-12 22:46:34 +0800

    ShopMall_uni-app

---
+++ pages/index/index.vue
@@ -0,0 +1,21 @@
+<template>
+	<view class="home-container">
+		zhuye
+	</view>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+import {onReady} from '@dcloudio/uni-app'
+const a = ref('123')
+onReady(()=>{
+	console.log("show");
+})
+</script>
+
+<style scoped>
+.home-container {
+	display: flex;
+	flex-direction: column;
+}
+</style>
